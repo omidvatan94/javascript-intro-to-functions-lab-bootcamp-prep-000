ae17e929df3d32d4ac721d7efd831e650ce9b3c7
Omid Vatandoost <omidvatan94@gmail.com>
2018-08-21 19:20:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ function sayHiToGrandma(string) {
   } else if (string === string.toUpperCase()) {
     return "YES INDEED!"
   } else {
-    return "I love you, too"
+    return "I love you, too."
   }}
